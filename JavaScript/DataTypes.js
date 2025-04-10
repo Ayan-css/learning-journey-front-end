562c496ea98e9f0e6cf83dda8c636bae90fe893e
Ayan-css <ayan48311@gmail.com>
2025-04-10 17:09:31 +0000

    types of data in javaScript

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,37 @@
+// DATA TYPES IN JS 
+ /*
+ 
+ String
+ Number 
+ Boolean
+ Undefined 
+ Null
+ Object
+ Symbol
+ Bigint
+
+ */
+
+ // String
+
+ let rname = "ayan"
+
+ // Number 
+
+ let age = 19
+
+ //Boolean 
+
+ let GoodPerson = true
+
+ //Object 
+  let TeaType = ["green tea", "black tea","milk tea"]
+
+  //we also get Data from another Date as example below
+
+  let whatName = rname;
+
+  // we print those Date using console 
+  // to look at printed data we have to go to terminal
+
+  console.log(rname)
