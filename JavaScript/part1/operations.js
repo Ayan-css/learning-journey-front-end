b0586c0c841f43c50a183ec282daee01c31e7be7
Ayan-css <ayan48311@gmail.com>
2025-04-10 17:56:58 +0000

    operators

---
+++ JavaScript/part1/operations.js
@@ -0,0 +1,71 @@
+// we can also perform operations in javaScript using operator 
+// Examples are below 
+
+//addtion 
+
+let addtion = 4 + 4
+
+console.log(addtion)
+
+let score = 30
+let bonus = 8
+let totalScore = score + bonus
+
+console.log(totalScore)
+
+
+// substraction 
+
+let substract = 5 - 2
+ 
+console.log(substract)
+
+//multiplacation
+
+let mult = 3 * 5
+console.log(mult)
+
+//devision 
+
+let div = 4/2
+console.log(div)
+
+// reminder
+
+let reminder = 8 % 2 
+console.log(reminder)
+
+// exponation
+
+let expo = 2 ** 3
+console.log(expo)
+
+//increment & decremeent 
+
+let budget = 2000
+budget++
+
+let credits = 500
+credits --
+
+// camparion operation
+
+let num1 = 3
+let num2 = 3
+let num3 = 30
+
+//it check if other is equal to another
+// it gives boolean value
+console.log(num1 == num2)
+console.log(num2 == num3)
+
+// it checks if other is not equal to another
+// it gives boolean value
+
+console.log(num1 != num2)
+console.log(num2 != num3)
+
+// to check greater than 
+
+console.log(num1 > num3)
+console.log(num1 < num3)
