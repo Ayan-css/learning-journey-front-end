7fbf36f885496642ed38a94b3625795973ee5411
Ayan-css <ayan48311@gmail.com>
2025-04-10 17:23:32 +0000

    changes and  constant

---
+++ JavaScript/changes.js
@@ -0,0 +1,12 @@
+let gameName = "GTA5"
+// we can rewrite the value of let variable
+gameName = "GTA6"
+
+// we can use const variable to make permanent memmory 
+
+const username = "Ayan-css"
+
+//once i have declared const variable i can not rewrite it
+
+// check the terminal
+// const username = "code_with_affan"  /*fisrt uncomment this line*/ 
