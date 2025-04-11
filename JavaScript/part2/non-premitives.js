d69778a5ddf75d069afa749528e6f6ea0a6c937e
Ayan-css <ayan48311@gmail.com>
2025-04-11 20:46:34 +0000

    objects

---
+++ JavaScript/part2/non-premitives.js
@@ -0,0 +1,23 @@
+let obj = {}
+
+console.log(obj)
+//objects uses larger memory compare to premitives
+
+// other style to write obj
+
+let username = {
+    "first name" : "ayan",
+    lastname : "ansari"
+}
+console.log(username["first name"])
+console.log(username.lastname)
+
+const data = {
+    lname : "ayan",
+    fname : "shaikh"
+}
+
+data.fname = "ansari" 
+console.log(data)
+console.log(data.fname)
+console.log(data.lname)
