6b4fa5ec236e5bba090b1f97ad8640299df847dd
Ayan-css <ayan48311@gmail.com>
2025-04-12 09:22:15 +0000

    arrays

--- JavaScript/part2/non-premitives.js
+++ JavaScript/part2/non-premitives.js
@@ -21,3 +21,19 @@ data.fname = "ansari"
 console.log(data)
 console.log(data.fname)
 console.log(data.lname)
+
+let today = new Date();
+
+console.log(today.getDate())
+
+//Array
+
+// let heros = ["a","b","c", true]
+let usergone = ["ayan",true]
+
+console.log(usergone[0])
+console.log(usergone[1])
+
+
+let isValue = true;  //true is considered as 1 and false is considered as 0
+console.log(isValue + 1)
