d9048d54f1350fb2492897f51d102998522ed608
Ayan-css <ayan48311@gmail.com>
2025-05-26 21:07:18 +0530

    create custom react and something

--- React/01viteReact/src/main.jsx
+++ React/01viteReact/src/main.jsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+/*we can create our own react element by using following methhod */
+const anotheruser = ' chai or react'
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'clcik me to visit google',
+  anotheruser
+)
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+   <>
+   {ReactElement}
+   <App />
+   {/* if we are combining two or more element then we have to use "{}" to run variables if we dont do this then it will be considerd as html */}
+   </>
   
 )
