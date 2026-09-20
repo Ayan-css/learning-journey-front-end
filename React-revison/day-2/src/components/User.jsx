586d5380d59176766ae57515ab2329afae7c71a1
Ayan <ayan48311@gmail.com>
2026-09-20 23:22:12 +0530

    components & props

---
+++ React-revison/day-2/src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function User({name,age}) {
+  return (
+    <>
+    <h1>Developer :{name}</h1>
+    <h2>Developer :{age}</h2>
+    </>
+  )
+}
+
+export default User  
